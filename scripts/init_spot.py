0a6cb5724db699373ec0c8a6604530a49119c706
Sylvain <sylvainjannin@gmail.com>
2022-06-22 09:36:26 +0100

    correct service

--- scripts/init_spot.py
+++ scripts/init_spot.py
@@ -2,7 +2,6 @@
 
 from __future__ import print_function
 
-import sys 
 import rospy 
 
 from std_srvs.srv import Trigger
@@ -10,18 +9,22 @@ from std_srvs.srv import Trigger
 def call_services(service_name):
     rospy.wait_for_service(service_name)
     try:
-        ret = rospy.ServiceProxy(service_name, Trigger)
-        print(ret)
+        order = rospy.ServiceProxy(service_name, Trigger)
+        response = order()
+        print(response)
         print(service_name, " works")
         return("")
-    except:
-        print(service_name, "error")
+    except rospy.ServiceException as e:
+        print("Service call failed: %s" %e)
+        print(service_name, "error") 
 
 
 def initialise_spot():
+    rospy.init_node('initialise_spot')
     call_services('/spot/claim')
     call_services('/spot/power_on')
     call_services('/spot/stand')
 
 if __name__ == '__main__':
+
     initialise_spot()
