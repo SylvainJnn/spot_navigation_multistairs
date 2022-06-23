e9d11e92053c717a45a2baedca64065502553c6d
root <k.judzentyte@hw.ac.uk>
2022-06-23 09:13:48 +0000

    trying to figure out how to add waypoints and connection

--- scripts/test.py
+++ scripts/test.py
@@ -5,7 +5,8 @@ from waypoints_manager import waypoints_manager
 
 
 if __name__ == '__main__':
-    rospy.init_node('test_node__waypoints_create')
-    new_wp = waypoints_manager()
+    #rospy.init_node('test_node__waypoints_create')
+    new_wp = waypoints_manager("C")
+    print("creating")
     new_wp.create_a_new_waypoint_robot_position()
 
--- scripts/waypoints_manager.py
+++ scripts/waypoints_manager.py
@@ -19,10 +19,17 @@ from navigation_2d_spot.srv import CreatePath, CreatePathResponse
 #this class exists to manage the waypoints: add new waypoints while moving 
 class waypoints_manager:
     
-    def __init__(self):
-        rospy.init_node('waypoints_manager')
+    def __init__(self, new_nb):
+        rospy.init_node('waypoints_manager_node')
         self.waypoint_counter = 0       #count the number of waypoints added
-        rospy.spin()
+        
+        self.nb = new_nb
+        rospy.wait_for_service('/rosplan_knowledge_base/update_array')
+        rospy.wait_for_service('/rosplan_problem_interface/problem_generation_server')
+        rospy.wait_for_service('/rosplan_planner_interface/planning_server')
+        rospy.wait_for_service('/rosplan_parsing_interface/parse_plan')
+        rospy.wait_for_service('/rosplan_plan_dispatcher/dispatch_plan')
+        #rospy.spin()
 
     def get_current_robot_pose_from_tf_listener(self):#from main function in replan.py
         print("get a poser from tf listener")
@@ -52,12 +59,13 @@ class waypoints_manager:
         waypoint.waypoint.pose.orientation.y = pose[1][1]
         waypoint.waypoint.pose.orientation.z = pose[1][2]
         waypoint.waypoint.pose.orientation.w = pose[1][3]
-    
+
+        print("the name is :", waypoint.id)    
         #waypoint.connecting_distance = 10 #?????
         try:
             # Add waypoint using service 
             add_waypoint = rospy.ServiceProxy('/rosplan_roadmap_server/add_waypoint', AddWaypoint)
-            print("Adding new home waypoint [ %s %s %s ] "%(round(pose[0][0], 2), round(pose[0][1], 2), pose[0][2]))
+            print("Adding NEWy waypoint [ %s %s %s ] "%(round(pose[0][0], 2), round(pose[0][1], 2), pose[0][2]))
             resp1 = add_waypoint(waypoint)
             return resp1
         except rospy.ServiceException as e:
@@ -69,8 +77,10 @@ class waypoints_manager:
 
         #to handle the name, for the moment let's say the program create newname different from the waypoints.yaml file
         #--> after let's make the program read that file and see which what's the new name
-        new_waypoint_name = "wp" + str(100 + self.waypoint_counter)#wp10X
+        #new_waypoint_name = "wp" + str(100) + str(self.waypoint_counter)#wp10X
+        new_waypoint_name = "wp" + str(self.nb)
 
         self.create_a_new_waypoint(pose, new_waypoint_name)
-        self.waypoint_counter += 1                                      #increment the variable
+        self.waypoint_counter = self.waypoint_counter + 1                                      #increment the varile
+        
 
