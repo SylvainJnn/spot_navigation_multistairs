ee81e7af094c09e0aafc997ce8e1059f3a9222a8
Sylvain <sylvainjannin@gmail.com>
2022-06-24 11:02:24 +0100

    update init

--- scripts/init_spot.py
+++ scripts/init_spot.py
@@ -24,7 +24,7 @@ def initialise_spot():
     call_services('/spot/claim')
     call_services('/spot/power_on')
     call_services('/spot/stand')
+    print("Initialisation is over")
 
 if __name__ == '__main__':
-
     initialise_spot()
