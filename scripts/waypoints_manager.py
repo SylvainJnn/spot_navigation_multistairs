b3abb7fa4a883d0c766c5bc6a444e41ce200e723
Sylvain <sylvainjannin@gmail.com>
2022-06-30 15:24:55 +0100

    add fucntion to add waypoints in front of robot current position

--- scripts/waypoints_manager.py
+++ scripts/waypoints_manager.py
@@ -14,6 +14,8 @@ from rosplan_interface_mapping.srv import *
 from rosplan_dispatch_msgs.srv import *
 
 from navigation_2d_spot.srv import CreatePath, CreatePathResponse
+#delete all unsed import please
+import numpy as np
 
 
 #this class exists to manage the waypoints: add new waypoints while moving 
@@ -23,6 +25,10 @@ class waypoints_manager:
         rospy.init_node('waypoints_manager_node')
         self.waypoint_counter = 0       #count the number of waypoints added
         self.nb = new_nb
+
+    def get_yaw_from_pose(self, pose):#get yaw's robot from pose from tf lsitener
+        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(pose[1])
+        return(yaw)
 
     def get_current_robot_pose_from_tf_listener(self):#from main function in replan.py #it take the robot pose form tf listener and return it 
         print("get a poser from tf listener")
@@ -35,7 +41,30 @@ class waypoints_manager:
             print('Fail')
             exit()
 
-    def create_a_new_waypoint(self, pose, pose_name):# setPose function from replan.py #//later do not tkae robot current pose but a position the robot see !
+    def get_pose_in_font_of(self):
+        robot_pose = self.get_current_robot_pose_from_tf_listener()#get robot pose
+        waypoint_pose = robot_pose                                 #for after we need the same format as robot_pose from tf.lsitener, but x and y are going to be changed
+        
+        yaw = self.get_yaw_from_pose(robot_pose)                                             #robot's yaw from pose
+        
+        #change base
+        waypoint_position_robot = np.array([2.0,0.0])   #x,y object position, we put in at 2 m in front of the robot
+        R = np.array([[np.cos(yaw),-1*(np.sin(yaw))],   
+                      [np.sin(yaw),    np.cos(yaw)]])   #transformation matrix
+        
+        robot_pose_step = np.array([robot_pose[0][0],
+                                    robot_pose[0][1]])  #we work with array like that = [x,y]
+        
+        #new_pose = robot_pose + R*waypoint_position_robot
+        new_pose = np.add(robot_pose_step, np.matmul(R,waypoint_position_robot))
+        
+        waypoint_pose[0][0] = new_pose[0] #x
+        waypoint_pose[0][1] = new_pose[1] #y
+
+        return(waypoint_pose)
+
+
+    def create_waypoint(self, pose, pose_name):# setPose function from replan.py #//later do not tkae robot current pose but a position the robot see !
         # New waypoint
         waypoint = AddWaypointRequest()#what is that object ??????????,
         waypoint.id = pose_name
@@ -69,11 +98,22 @@ class waypoints_manager:
         #--> after let's make the program read that file and see which what's the new name
         new_waypoint_name = "wp" + str(self.nb)                         #give the waypoint name 
 
+        self.create_waypoint(pose, new_waypoint_name)             #create the waypoint ins Rosplan
+        yaml_manager.add_waypoint_to_yaml_file(pose, new_waypoint_name)#add the waypoint in yaml file
+        #load the new waypoint to connect it : load_edges.bash #right now it deleteds the new home because it is not written in the yaml file // call this also when create new home ? // or get rd of home ? 
+        self.waypoint_counter = self.waypoint_counter + 1                                      #increment the varile
+
+    def create_a_new_waypoint_in_front_of(self):
+        rospy.wait_for_service('/rosplan_roadmap_server/add_waypoint')  #wait for the service to be availible before adding a new waypoint           
+        pose = self.get_pose_in_font_of()
+
+        new_waypoint_name = "wp" + str(self.nb)                         #give the waypoint name 
+
         self.create_a_new_waypoint(pose, new_waypoint_name)             #create the waypoint ins Rosplan
         yaml_manager.add_waypoint_to_yaml_file(pose, new_waypoint_name)#add the waypoint in yaml file
         #load the new waypoint to connect it : load_edges.bash #right now it deleteds the new home because it is not written in the yaml file // call this also when create new home ? // or get rd of home ? 
         self.waypoint_counter = self.waypoint_counter + 1                                      #increment the varile
-        
+
 
 if __name__ == '__main__':
     #rospy.init_node('test_node__waypoints_create')
