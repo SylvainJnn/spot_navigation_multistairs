6b12e80d2a1143c069938ceee3105705263cf634
Sylvain <sylvainjannin@gmail.com>
2022-07-01 10:21:04 +0100

    change the service type and add function to stop spot

--- scripts/init_spot.py
+++ scripts/init_spot.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 import rospy 
 from std_srvs.srv import Trigger
 
+
+#import this funciton from anotehr file
 #call the services, they are all called with Trigger message and it does not need feedback
 def call_services(service_name):
     rospy.wait_for_service(service_name)    
@@ -25,6 +27,14 @@ def initialise_spot():
     call_services('/spot/power_on')
     call_services('/spot/stand')
     print("Initialisation is over")
+
+def stop_spot():
+    rospy.init_node('stop_spot')
+    call_services('/spot/sit')
+    call_services('/spot/self_right')
+    call_services('/spot/power_off')
+    call_services('/spot/release')
+    print("stopping is over")
 
 if __name__ == '__main__':
     initialise_spot()
--- scripts/stairs_manager.py
+++ scripts/stairs_manager.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 import rospy 
-from std_srvs.srv import Trigger
+from std_srvs.srv import Trigger, SetBool
 
 
 
@@ -13,7 +13,7 @@ class stairs_mangager:
         None
 
     #call the services, they are all called with Trigger message and it does not need feedback
-    def call_services(service_name):
+    def call_services_Trigger(service_name):
         rospy.wait_for_service(service_name)    
         try:
             order = rospy.ServiceProxy(service_name, Trigger)
@@ -25,13 +25,25 @@ class stairs_mangager:
             print("Service call failed: %s" %e)
             print(service_name, "error") 
 
+    def set_stair_mode(service_name, state):
+        rospy.wait_for_service(service_name)    
+        try:
+            order = rospy.ServiceProxy(service_name, SetBool)
+            response = order(state)#call the service
+            print(response)
+            print(service_name, " works")
+            return(True)
+        except rospy.ServiceException as e:
+            print("Service call failed: %s" %e)
+            print(service_name, "error") 
 
     #call the different services to make spot availible for navigation
     def pass_to_stair_mode():
-        call_services("") #/spot/stairs (??)
+        set_stair_mode("/spot/stair_mode", True) #/spot/stairs (??)
         None
 
     def pass_to_normal_mode():
+        set_stair_mode("/spot/stair_mode", False) 
         None
 
     def allign_stair(self):
