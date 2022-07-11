81ba1c8b80022e8ace5a024a57818e53b5257c3c
Sylvain <sylvainjannin@gmail.com>
2022-07-11 15:08:27 +0100

    update waypoints coordinates and add comments

--- scripts/graph/load_edges.bash
+++ scripts/graph/load_edges.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #$1 --> waypoints_2. --> `rospack find spot_navigation_multistairs`/config/waypoints/$1.yaml 	
-
+#it looks like it is not getting called ? # called independatly it is ok 
 rosparam load `rospack find spot_navigation_multistairs`/config/waypoints/waypoints_2.yaml 	
 
 rosservice call /rosplan_roadmap_server/load_waypoints
--- scripts/load_edges.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rosparam load `rospack find spot_navigation_multistairs`/config/waypoints_test.yaml 	
-
-rosservice call /rosplan_roadmap_server/load_waypoints
