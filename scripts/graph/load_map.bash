82a25b9a231f8642ed638610e2b25a105741f728
Sylvain <sylvainjannin@gmail.com>
2022-07-14 14:54:57 +0100

    less hardcoded

--- scripts/graph/load_map.bash
+++ scripts/graph/load_map.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+#this script launch a new map_server node with a different map
+rosnode kill /map_server
+map="`rospack find spot_navigation_multistairs`/maps/$1.yaml"
 
-
-roslaunch spot_navigation_multistairs map_server.launch map_file:=$1
+roslaunch spot_navigation_multistairs map_server.launch map_file:=$map
