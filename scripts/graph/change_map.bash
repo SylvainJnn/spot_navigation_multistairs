c47e378e64041d4b35864f26330911b1dec19934
Sylvain <sylvainjannin@gmail.com>
2022-07-15 12:26:02 +0100

    get back bash scripts

---
+++ scripts/graph/change_map.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./delete_graph.bash
+
+#to lead a map, take the first argument
+./load_map.bash $1
+
+#second argument is waypoints file
+./load_edges $2 
---
+++ scripts/graph/load_map.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+#this script launch a new map_server node with a different map
+rosnode kill /map_server
+map="`rospack find spot_navigation_multistairs`/maps/$1.yaml"
+roslaunch spot_navigation_multistairs map_server.launch map_file:=$map
