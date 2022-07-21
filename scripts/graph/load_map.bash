ace001d79151d942ec9881c29006027a27ebff95
Sylvain <sylvainjannin@gmail.com>
2022-07-21 13:06:06 +0100

    stop killing node to change map and use service // add chmod+x

--- scripts/graph/load_map.bash
+++ scripts/graph/load_map.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 #this script launch a new map_server node with a different map
-rosnode kill /map_server
+#rosnode kill /map_server
 map="`rospack find spot_navigation_multistairs`/maps/$1.yaml"
-roslaunch spot_navigation_multistairs map_server.launch map_file:=$map
+#roslaunch spot_navigation_multistairs map_server.launch map_file:=$map
+
+#-------------------------------------
+rosservice call /change_map map
