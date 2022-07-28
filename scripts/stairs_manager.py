6d0ea1ab583f902b80ef2415b90f49a6da22ee0e
Sylvain <sylvainjannin@gmail.com>
2022-07-28 10:50:29 +0100

    update for 1st demo

--- scripts/stairs_manager.py
+++ scripts/stairs_manager.py
@@ -8,9 +8,19 @@ from std_srvs.srv import Trigger, SetBool
 
 
 class stairs_mangager:
-    def __init__(self):
+    def __init__(self, mode):
         rospy.init_node('spot_stairs_manager')
-        None
+        self.get_stairs_mode(mode)
+        print("start")
+        self.set_stair_mode()
+        print("done ")
+    
+    def get_stairs_mode(self, mode):#set thje stair mode, if the global parameter is Normal --> False. the service set spot to stair mode if the given argument is True // to normal mode if argument is false
+        if(mode == "normal"):
+            self.stairs_mode = False
+        else:
+            self.stairs_mode = True
+
 
     #call the services, they are all called with Trigger message and it does not need feedback
     def call_services_Trigger(service_name):
@@ -25,12 +35,12 @@ class stairs_mangager:
             print("Service call failed: %s" %e)
             print(service_name, "error") 
 
-    def set_stair_mode(self, state):
+    def set_stair_mode(self):
         service_name = "/spot/stair_mode"
         rospy.wait_for_service(service_name)    
         try:
             order = rospy.ServiceProxy(service_name, SetBool)
-            response = order(state)#call the service
+            response = order(self.stairs_mode)#call the service
             print(response)
             print(service_name, " works")
             return(True)
@@ -55,10 +65,6 @@ class stairs_mangager:
 
 #normally it works
 if __name__ == '__main__':
-    yo = stairs_mangager()
-    print("start")
-    yo.pass_to_stair_mode()
-    rospy.sleep(5)
-    yo.pass_to_stair_mode()
-    print("over")
+    mode = rospy.get_param("mode") #get stair mode "normal or other"
+    print("really over")
     None
